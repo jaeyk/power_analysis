7db98bd760d5f8f03c9d813742c1288e7f281ca8
Jae Yeon Kim <jaeyeonkim@codeforamerica.org>
2024-01-17 00:29:54 -0800

    add two more moderators

--- functions/utils.R
+++ functions/utils.R
@@ -11,6 +11,8 @@ get_db_rl_estimates <- function(treated_n) {
   df_sub$china_threatend_high <- factor(df_sub$china_threatend_high) 
   df_sub$DEM <- factor(df_sub$DEM) 
   df_sub$GOP <- factor(df_sub$GOP) 
+  df_sub$immigrant <- factor(df_sub$immigrant) 
+  df_sub$college <- factor(df_sub$college) 
   
   # known propensity score
   df_sub <- df_sub %>%
@@ -21,7 +23,7 @@ get_db_rl_estimates <- function(treated_n) {
     add_known_propensity_score("ps") %>% # known because this is an experiment 
     add_outcome_model("SL.glmnet") %>% # ensemble of machine learning models 
     # discrete
-    add_moderator("Stratified", chinese_origin, democracy_cat, china_threatend_high, DEM, GOP) %>%
+    add_moderator("Stratified", chinese_origin, democracy_cat, china_threatend_high, DEM, GOP, immigrant, college) %>%
     # continuous
     #add_moderator("KernelSmooth", china_threat_index) %>%
     add_vimp(sample_splitting = FALSE) -> hte_cfg
@@ -38,12 +40,14 @@ get_db_rl_estimates <- function(treated_n) {
       democracy_cat, # x2 
       china_threatend_high, # x3 
       DEM, # x4 
-      GOP # x5
+      GOP, # x5
+      immigrant, 
+      college
     ) %>%
     construct_pseudo_outcomes(racial_linked_fate, treated) -> df_sub_splits 
   
   df_sub_splits %>%
-    estimate_QoI(chinese_origin, democracy_cat, china_threatend_high, DEM, GOP) -> results
+    estimate_QoI(chinese_origin, democracy_cat, china_threatend_high, DEM, GOP, immigrant, college) -> results
   
   return(results)
 }
@@ -61,6 +65,8 @@ get_db_el_estimates <- function(treated_n) {
   df_sub$china_threatend_high <- factor(df_sub$china_threatend_high) 
   df_sub$DEM <- factor(df_sub$DEM) 
   df_sub$GOP <- factor(df_sub$GOP) 
+  df_sub$immigrant <- factor(df_sub$immigrant) 
+  df_sub$college <- factor(df_sub$college) 
   
   # known propensity score
   df_sub <- df_sub %>%
@@ -71,7 +77,7 @@ get_db_el_estimates <- function(treated_n) {
     add_known_propensity_score("ps") %>% # known because this is an experiment 
     add_outcome_model("SL.glmnet") %>% # ensemble of machine learning models 
     # discrete
-    add_moderator("Stratified", chinese_origin, democracy_cat, china_threatend_high, DEM, GOP) %>%
+    add_moderator("Stratified", chinese_origin, democracy_cat, china_threatend_high, DEM, GOP, immigrant, college) %>%
     # continuous
     #add_moderator("KernelSmooth", china_threat_index) %>%
     add_vimp(sample_splitting = FALSE) -> hte_cfg
@@ -88,12 +94,14 @@ get_db_el_estimates <- function(treated_n) {
       democracy_cat, # x2 
       china_threatend_high, # x3 
       DEM, # x4 
-      GOP # x5
+      GOP, # x5
+      immigrant, 
+      college
     ) %>%
     construct_pseudo_outcomes(ethnic_linked_fate, treated) -> df_sub_splits 
   
   df_sub_splits %>%
-    estimate_QoI(chinese_origin, democracy_cat, china_threatend_high, DEM, GOP) -> results
+    estimate_QoI(chinese_origin, democracy_cat, china_threatend_high, DEM, GOP, immigrant, college) -> results
   
   return(results)
 }
